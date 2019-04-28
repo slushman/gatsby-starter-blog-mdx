7f0cd324382ec371858d644409f62f2360513f5c
Chris Wilcoxson <chris@slushman.com>
2019-04-27 23:11:39 -0600

    Convert all components to functional components.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import SEO from '../components/SEO';
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <Layout location={ this.props.location }>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+const NotFoundPage = ( { location } ) => {
+  return (
+    <Layout location={ location }>
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
 };
 
 export default NotFoundPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,43 +3,40 @@ import { Link, graphql } from 'gatsby';
 
 import Bio from '../components/Bio';
 import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import SEO from '../components/SEO';
 import { rhythm } from '../utils/typography';
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMdx.edges;
+const BlogIndex = ( { data, location } ) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMdx.edges;
 
-    return (
-      <Layout location={ this.props.location } title={ siteTitle }>
-        <SEO
-          title="All posts"
-          keywords={ [ `blog`, `gatsby`, `javascript`, `react` ] }
-        />
-        <Bio />
-        { posts.map( ( { node } ) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={ node.fields.slug }>
-              <h3
-                style={ {
-                  marginBottom: rhythm( 1 / 4 ),
-                } }
-              >
-                <Link style={ { boxShadow: `none` } } to={ node.fields.slug }>
-                  { title }
-                </Link>
-              </h3>
-              <small>{ node.frontmatter.date }</small>
-              <p dangerouslySetInnerHTML={ { __html: node.excerpt } } />
-            </div>
-          )
-        } ) }
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={ location } title={ siteTitle }>
+      <SEO
+        title="All posts"
+        keywords={ [ `blog`, `gatsby`, `javascript`, `react` ] }
+      />
+      <Bio />
+      { posts.map( ( { node } ) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <div key={ node.fields.slug }>
+            <h3
+              style={ {
+                marginBottom: rhythm( 1 / 4 ),
+              } }
+            >
+              <Link style={ { boxShadow: `none` } } to={ node.fields.slug }>
+                { title }
+              </Link>
+            </h3>
+            <small>{ node.frontmatter.date }</small>
+            <p dangerouslySetInnerHTML={ { __html: node.excerpt } } />
+          </div>
+        )
+      } ) }
+    </Layout>
+  )
 };
 
 export default BlogIndex;
